129317bc2a8d3449ad536baa0762e19ca983699e
Haruokuma <e107ti@etec.sp.gov.br>
2023-08-18 20:49:04 -0300

    Concluido

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Sol aparece");
+﻿Console.WriteLine("Sol aparece");
 Console.WriteLine("Flores florescem");
 Console.WriteLine("Borboletas enaltecem");
